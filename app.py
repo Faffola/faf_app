deacb2c616121f46bd0f25097443b890049f7caa
Faffola <raffymero@gmail.com>
2016-09-26 17:25:16 +0000

    updated controllers

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 # initialization
 app = Flask(__name__)
@@ -16,3 +16,16 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
